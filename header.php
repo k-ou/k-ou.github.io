07c94864d15ddc7ce8d3895308b95a098cc3da8d
k-ou <kellio94@ucla.edu>
2015-12-30 16:55:18 -0800

    first commit

---
+++ header.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html>
+
+<body>
+        
+    <nav class="navbar navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="./query.php">
+                    <img alt="placeholder icon" src="http://www.i2symbol.com/images/abc-123/k/circled_latin_small_letter_k_u24DA_icon_256x256.png">
+                </a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="./index.html">Hello</a></li>
+                    <li><a href="./about.html">It's me</a></li>
+                    <li><a href="./index.html">I was wondering if after all these years you'd like to meet</a></li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+    
+</body>
+    
+</html>
