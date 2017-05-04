1f185e3017c5e38dfc1a39d579cee0a976c8df06
k-ou <kellio94@ucla.edu>
2017-05-03 22:49:19 -0700

    Working parallax in intro

--- scca/js/main.js
+++ scca/js/main.js
@@ -76,3 +76,26 @@
             }
         });
     });
+
+    // parallax for falling white people
+    $(document).ready(function () {
+        //$('html, body').animate({
+        //		scrollTop: $('div#bg-3-4').offset().top+700
+        //}, 15000);
+
+        //	$(document).click(function(){
+        //		$('html, body').stop(true);
+        //	});
+
+        $(window).scroll(function (e) {
+            parallaxScroll();
+        });
+
+        function parallaxScroll() {
+            var scrolled = $(window).scrollTop() - $(".white_guys").offset().top - 850;
+            $('#parallax-bg-1').css('top', (0 - (scrolled * -1.4)) + 'px');
+            $('#parallax-bg-2').css('top', (0 - (scrolled * -1.1)) + 'px');
+            $('#parallax-bg-3').css('top', (0 - (scrolled * -.75)) + 'px');
+        }
+
+    });
